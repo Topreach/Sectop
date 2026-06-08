d13b8dd47e7d47fc885a79fbfa055b60db03ebd5
Topreach <topreach@example.com>
2026-06-08 10:26:03 +0100

    Description of changes

--- backend/src/main/java/com/dangeremergence/config/RedisConfig.java
+++ backend/src/main/java/com/dangeremergence/config/RedisConfig.java
@@ -1,14 +1,25 @@
 package com.dangeremergence.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${REDIS_HOST:redis}") String redisHost,
+            @Value("${REDIS_PORT:6379}") int redisPort) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
