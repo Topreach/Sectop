6829dc664162b812a4319de79171edb2ac6a2797
Topreach <topreach@example.com>
2026-06-23 08:10:29 +0100

    fix: add missing MockBean import to DroneControllerTest, PublicControllerTest, DigitalTwinControllerTest

--- backend/src/test/java/com/dangeremergence/controller/PublicControllerTest.java
+++ backend/src/test/java/com/dangeremergence/controller/PublicControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
